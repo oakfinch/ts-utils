faa7cd10b14c41627aa81d2bd79881bafd31b181
Nate Volker <natevolker@gmail.com>
2021-10-01 14:15:40 -0500

    use shared tsconfig, eslint, and jest base config files

--- jest.config.js
+++ jest.config.js
@@ -1,11 +1,3 @@
-/** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
 module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'node',
-  collectCoverage: true,
-  passWithNoTests: true,
-  collectCoverageFrom: [
-    "src/**/*.{js,ts}",
-    "!**/__test__/**",
-  ],
+  preset: '@oakfinch/jest-config',
 };
