ca451b3ac417122ce9edf7b3b3a0d72c823ddf71
Nate Volker <natevolker@gmail.com>
2021-10-08 11:36:56 -0500

    adding documentation for

--- src/quote.ts
+++ src/quote.ts
@@ -1,3 +1,17 @@
+/**
+ * Wraps a string in quotes
+ *
+ * @param str the string to wrap
+ * @param lquo left quote
+ * @param rquo right quote
+ * @returns the quoted string
+ * @example
+ * ```
+ * quote('hello') // returns `"hello"`
+ * quote('hello', '#') // returns `#hello#`
+ * quote('hello', '<', '>') // returns `<hello>`
+ * ```
+ */
 export const quote = (
   str: string,
   lquo = '"',
