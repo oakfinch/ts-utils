4f634abfc432c5444269546ab1b8f7d88ac236c6
Nate Volker <natevolker@gmail.com>
2021-10-15 18:03:15 -0500

    adding isNullish type guard

--- src/type-guards/index.ts
+++ src/type-guards/index.ts
@@ -2,4 +2,5 @@ export * from './has-own-property';
 export * from './is-boolean';
 export * from './is-function';
 export * from './is-included';
+export * from './is-nullish';
 export * from './is-promise';
