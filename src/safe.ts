84e9604f3080a746426da176befcda630c019c01
Nate Volker <natevolker@gmail.com>
2021-10-15 10:55:09 -0500

    adding 'safe' function helper

---
+++ src/safe.ts
@@ -0,0 +1,29 @@
+/**
+ * A helper for handling awkward try/catch blocks
+ *
+ * @param fn - a function that has no arguments
+ * @returns the return value of `fn`, or `undefined` if `fn` throws an error
+ * @example
+ * ```
+ * // instead of:
+ * let value: string;
+ * try {
+ *   value = getValue(12345);
+ * } catch (error) {
+ *   value = 'some fallback';
+ * }
+ *
+ * // you can do:
+ * const value = safe(() => getValue(12345)) ?? 'some fallback';
+ *
+ * ```
+ */
+export const safe = <T>(fn: () => T): (T | undefined) => {
+  try {
+    return fn();
+  } catch {
+    return undefined;
+  }
+};
+
+export default safe;
