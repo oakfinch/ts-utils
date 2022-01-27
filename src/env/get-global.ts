2862f597dd3147081940f010c94bd27b73665145
Nate Volker <natevolker@gmail.com>
2022-01-27 15:52:28 -0600

    fix: fix getGlobal type

--- src/env/get-global.ts
+++ src/env/get-global.ts
@@ -1,5 +1,5 @@
 /* eslint-disable no-restricted-globals */
-export const getGlobal = (): Window => {
+export const getGlobal = (): Window & typeof globalThis => {
   if (typeof self !== 'undefined') {
     return self
   }
@@ -7,7 +7,7 @@ export const getGlobal = (): Window => {
     return window
   }
   if (typeof global !== 'undefined') {
-    return global as unknown as Window
+    return global as unknown as Window & typeof globalThis
   }
   throw new Error('unable to locate global object')
 }
